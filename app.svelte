963d9e0d4b019c4d1390a705358d869ce2a93510
asakew <asauz777@gmail.com>
2022-01-20 22:00:10 +0500

    import files

--- app.svelte
+++ app.svelte
@@ -1,8 +1,13 @@
 <script>
+  import Nested from './Nested.svelte';
   let siteName = 'app.uz';
 	let src = '/tutorial/image.gif';
 </script>
 
 <h1>{siteName}</h1>
+
 <img src={src}>
 
+<p>This is a paragraph.</p>
+
+<Nested />
